6ba74d6e3b64e080cc8493ca92f88d077f29b08b
Lion-ofJudah <metsakalz@gmail.com>
2025-12-15 18:35:12 +0300

    feat: configure telescope-ui-select.

---
+++ lua/lion/plugins/telescope/telescope-ui-select.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-telescope/telescope-ui-select.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    extensions = {
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown{}
+      }
+    },
+  },
+  config = function (_, opts)
+    require("telescope").setup(opts)
+    require("telescope").load_extension("ui-select")
+  end,
+}
