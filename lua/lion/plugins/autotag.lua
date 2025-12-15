0c7ade9a3dad1fe9b14e815834669357b18a374c
Lion-ofJudah <metsakalz@gmail.com>
2025-12-15 17:37:34 +0300

    feat: configure autotag and autopairs for tag and pair completion.

---
+++ lua/lion/plugins/autotag.lua
@@ -0,0 +1,18 @@
+return {
+  "windwp/nvim-ts-autotag",
+  opts = {
+    ft = { "html", "javascriptreact", "typescriptreact", "xml" },
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false,
+    },
+    aliases = {
+      ["javascriptreact"] = "html",
+      ["typescriptreact"] = "html",
+    },
+  },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
